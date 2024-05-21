6a7b4667e8896ea7200a2ec54e470eef969693b4
Sergey Golobokov <sergeygolobokov@gmail.com>
2024-05-21 17:43:28 +1000

    dd

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,48 +1,37 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView
+)
+from django.urls import reverse_lazy
 
 from .models import Birthday
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_date = form.cleaned_data['birthday']
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(
-        request, 'birthday/birthday.html', context
-    )
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
-def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
-    return render(
-        request, 'birthday/birthday_list.html', context
-    )
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
 
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(
-        request, 'birthday/birthday.html', context
-    )
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
